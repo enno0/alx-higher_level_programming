cf73a1eb8dc6b4df33242f389d619c25fe8ece15
Kamal Mohamed <kamalaldeenmohamed@gmail.com>
2023-11-29 03:35:47 +0200

    py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join(f"{chr(ch) if ch % 2 == 0 else chr(ch - 32)}" for ch in reversed(range(97, 123))))
+for ch in reversed(range(97, 123)):
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
