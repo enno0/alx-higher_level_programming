67646cf68ce20a148ba61a5feb772ddbeb7ac413
Kamal Mohamed <kamalaldeenmohamed@gmail.com>
2023-11-30 16:08:26 +0200

    dl3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
+
 if __name__ == "__main__":
-    
-        kamal=len(argv) - 1
-        plural_s = "s" if kamal != 1 else ""
-        print("{} argument{}".format(kamal, plural_s),end="")
-        print("{}".format(":" if kamal > 0 else "."))
-        for i in range(kamal):
-             print("{:d}: {}".format(i + 1, argv[i + 1]))
-        
+    kamal = len(argv) - 1
+    plural_s = "s" if kamal != 1 else ""
+    print(f"{kamal} argument{'s' if kamal != 1 else ''}", end="")
+    print("{}".format(":" if kamal > 0 else "."))
+    for i in range(kamal):
+        print("{:d}: {}".format(i + 1, argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
+
 if __name__ == "__main__":
-   args = argv[1:]
-   d=sum(int(arg) for arg in args)
-   print("{}".format(d))
+    args = argv[1:]
+
+    result = sum(int(arg) for arg in args)
+
+    print("{}".format(result))
