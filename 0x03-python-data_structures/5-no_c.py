556b28e11642b94ed08f63afb972823c81f9ec46
Kamal Mohamed <kamalaldeenmohamed@gmail.com>
2023-12-03 17:21:26 +0200

    py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    my_string2 = ""
+    for char in my_string:
+        if char.lower() != 'c' or char.upper() != 'C':
+            my_string2 = my_string2 + char
+    return my_string2
