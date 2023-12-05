721a58d7590d1c11d4b4725131756676a106f2f3
Kamal Mohamed <kamalaldeenmohamed@gmail.com>
2023-12-05 03:43:10 +0200

    adding delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,6 +2,15 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    result_list = my_list[:idx] + my_list[idx+1:]
-
+    result_list = []
+    for i in range(len(my_list)):
+        if i != idx:
+            result_list.append(my_list[i])
     return result_list
+
+
+
+original_list = [1, 2, 3, 4, 5]
+index_to_delete = 2
+result = delete_at(original_list, index_to_delete)
+print(result)
