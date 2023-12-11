0aa480b2d18d646387385adf63c51a8b04b4123a
Kamal Mohamed <kamalaldeenmohamed@gmail.com>
2023-12-11 07:03:34 +0200

    springboot_mysql

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_integers = set()
+
+    for element in my_list:
+        unique_integers.add(element)
+    result_sum = sum(unique_integers)
+
+    return result_sum
