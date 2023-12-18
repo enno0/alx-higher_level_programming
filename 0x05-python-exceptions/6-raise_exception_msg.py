efbabb57bf7add0ed4d64645e904b2d8447a75dc
Kamal Mohamed <kamalaldeenmohamed@gmail.com>
2023-12-19 01:07:45 +0200

    py

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -3,5 +3,4 @@ def raise_exception_msg(message=""):
     try:
         undefined_variable
     except NameError as e:
-        print(message)
-        raise 
+        raise NameError(message) from e
