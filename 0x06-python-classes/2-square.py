933221639050c8ba03e10d236ac9aee33d49a80c
Kamal Mohamed <kamalaldeenmohamed@gmail.com>
2023-12-21 02:27:50 +0200

    py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0):
+        """Class constructor with optional size parameter"""
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
