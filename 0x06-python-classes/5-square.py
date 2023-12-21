fd4bd6615b2a1f2cb5f74c684ef9bc99eb9af285
Kamal Mohamed <kamalaldeenmohamed@gmail.com>
2023-12-21 02:51:36 +0200

    py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """Class constructor with optional size parameter"""
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """Property getter for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property setter for size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """Public instance method to calculate and return the area """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Public instance method to print the square with # characters"""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
