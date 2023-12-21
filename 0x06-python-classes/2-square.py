140fbe6a5a3209f672a73c898be0db43c654474f
Kamal Mohamed <kamalaldeenmohamed@gmail.com>
2023-12-21 02:31:44 +0200

    py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+"""This module defines a Square class."""
+
+
 class Square:
     """Square class"""
-
     def __init__(self, size=0):
         """Class constructor with optional size parameter"""
         self.__size = size
