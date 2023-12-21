01992831fd0dd1e7fe65623bf583c0b399b9261f
Kamal Mohamed <kamalaldeenmohamed@gmail.com>
2023-12-21 02:37:49 +0200

    py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""This module defines a Square class."""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """Class constructor with optional size parameter"""
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Public instance method to calculate and return the area"""
+        return self.__size ** 2
